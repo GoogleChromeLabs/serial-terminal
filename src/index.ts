ab4391756917dc34f7a12e21611dda567b40d0b7
Reilly Grant <me@reilly.io>
2020-03-20 15:30:14 -0700

    Specify the service worker registration scope
    Fixes #6.

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
       const registration =
-          await navigator.serviceWorker.register('/service-worker.js');
+          await navigator.serviceWorker.register('service-worker.js',
+                                                 { scope: '.' });
       console.log('SW registered: ', registration);
     } catch (registrationError) {
       console.log('SW registration failed: ', registrationError);
