85bb3e4df785e671c6c19b6a1e7b169d4c72e116
Reilly Grant <reillyg@google.com>
2022-10-26 16:56:07 -0700

    Set publicPath to '/' for webpack-dev-server
    Otherwise a "Cannot GET /" error results.

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    publicPath: '/',
+  },
 });
