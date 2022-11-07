a76da3acf61b7272a55b7301a024149b4070eec7
Reilly Grant <reillyg@google.com>
2022-11-07 15:43:58 -0800

    Migrate from webpack to Vite

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import eslint from 'vite-plugin-eslint'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default {
+  plugins: [
+    eslint(),
+    VitePWA({
+      manifest: {
+        short_name: "Serial Terminal",
+        name: "Serial Terminal",
+        icons: [
+          {
+            src: "images/icons-1024.png",
+            type: "image/png",
+            sizes: "1024x1024",
+            purpose: "any maskable"
+          },
+          {
+            src: "images/icons-192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any maskable"
+          },
+          {
+            src: "images/icons-512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable"
+          }
+        ],
+        "start_url": "./?source=pwa",
+        "display": "standalone",
+        "scope": "./"
+      }
+    })
+  ]
+}
