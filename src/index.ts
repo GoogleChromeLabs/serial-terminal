9acabcdbf12b035842a68f008db349ef1307662c
Reilly Grant <me@reilly.io>
2026-05-23 00:16:38 -0700

    Update to xterm.js 6.0.0

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-import {Terminal} from 'xterm';
-import {FitAddon} from 'xterm-addon-fit';
-import {WebLinksAddon} from 'xterm-addon-web-links';
-import 'xterm/css/xterm.css';
+import {Terminal} from '@xterm/xterm';
+import {FitAddon} from '@xterm/addon-fit';
+import {WebLinksAddon} from '@xterm/addon-web-links';
+import '@xterm/xterm/css/xterm.css';
 import {
   serial as polyfill, SerialPort as SerialPortPolyfill,
 } from 'web-serial-polyfill';
